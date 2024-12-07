cc3dac875e89ee22a009ffe0ab625e8888d5d390
Anelsonmousse <138482174+Anelsonmousse@users.noreply.github.com>
2024-12-07 09:43:18 +0100

    Update sanityClient.js

--- src/app/sanity/sanityClient.js
+++ src/app/sanity/sanityClient.js
@@ -1,11 +1,11 @@
 import { createClient } from "@sanity/client";
 
 const client = createClient({
-  projectId: "03uv5l67", // replace with your Sanity project ID
+  projectId: "xrqp3rws", // replace with your Sanity project ID
   dataset: "production", // replace with your Sanity dataset
   // apiVersion: '2022-08-08',
   token:
-    "sk8F36qpuysDMQBjPjkARoLI21WsAbSKzaz845K85Uif43fKVya5Z8rktWF9UuaSbhTErejEJQUUHZzBoLRXJsnNwnfsCX5VZhLYJRQUAngldWR0o1IvhGwDdqG1FBBEV46OhD9ZTpd0INeCrosvGc2WaqfnxDilCwa65hfPAMWlC9zZTfjm", // replace with your Sanity token
+    "skFSYtRNLPBlwFbKeVt0AwKg6ZJx34sePVeU30IJHOmohW2MS6TBj0pqgUEaIhpmeALW4PSXEIPW6qTWalH9Pxsv5GAQhftOMbujtj5JkcfsoJNorgMbuAqFVMNjghTOwfDL5pgpMb27BqfwiuRiBydOKuEBSMkaX8pxc8smr8ytzKFxA8nV", // replace with your Sanity token
   useCdn: false, // `false` if you want to ensure fresh data
 });
 
