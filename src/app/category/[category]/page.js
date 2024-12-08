db7d88a936bd9329a64c0cfb440fb7ef11eb226c
Lone Wolf <110489493+Loneewolf15@users.noreply.github.com>
2024-12-08 13:15:35 -0800

    Update page.js

--- src/app/category/[category]/page.js
+++ src/app/category/[category]/page.js
@@ -162,11 +162,17 @@ const Page = () => {
     return (
         <main className="pt-8 px-2">
            
-                  <div className="flex justify-center items-center w-full h-20">
-      <h1 className="text-2xl font-bold text-center">
-        {formattedName}
-      </h1>
-    </div>
+    //               <div className="flex justify-center items-center w-full h-20">
+    //   <h1 className="text-2xl font-bold text-center">
+    //     {formattedName}
+    //   </h1>
+    // </div>
+        <h1 className="text-2xl font-bold mb-4 text-center">
+            {categoryName 
+                ? categoryName[0].toUpperCase() + categoryName.slice(1).toLowerCase()
+                : "Products"}
+        </h1>
+            
             <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
                 {products.length > 0 ? (
                     products.map((product, index) => (
