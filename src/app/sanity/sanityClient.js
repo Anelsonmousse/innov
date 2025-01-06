ad19c15e5b5a49a419f118ef10a70fd2e85acd88
Anelsonmousse <138482174+Anelsonmousse@users.noreply.github.com>
2025-01-06 17:00:13 +0100

    Update sanityClient.js

--- src/app/sanity/sanityClient.js
+++ src/app/sanity/sanityClient.js
@@ -5,7 +5,7 @@ const client = createClient({
   dataset: "production", // replace with your Sanity dataset
   // apiVersion: '2022-08-08',
   token:
-    "skFSYtRNLPBlwFbKeVt0AwKg6ZJx34sePVeU30IJHOmohW2MS6TBj0pqgUEaIhpmeALW4PSXEIPW6qTWalH9Pxsv5GAQhftOMbujtj5JkcfsoJNorgMbuAqFVMNjghTOwfDL5pgpMb27BqfwiuRiBydOKuEBSMkaX8pxc8smr8ytzKFxA8nV", // replace with your Sanity token
+    "skQMCrvfDQR63mvPChnvnM3joBs6myKydXvCuX1BR3zbF145uI1OMeKj4Wpmg2z44zQbST4ZAmjIg5TaKISv4wOzQTcCMwq8rSw7Zc8yslPrfq9SxcylyNaKofgXA3ENJLi1fbfvE2zwyBndL9NvR2o8sBlgsMjFUjNW5ZSrtjse1UZkMEl7", // replace with your Sanity token
   useCdn: false, // `false` if you want to ensure fresh data
 });
 
